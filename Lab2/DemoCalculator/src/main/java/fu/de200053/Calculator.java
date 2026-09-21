fb6d0116d96255638dadf712d9ceb13acfdaea9c
Duc Pham <mpham9495@gmail.com>
2026-09-21 13:36:26 +0700

    feat(DemoCalculator): viet unit test cho lop Calculator

---
+++ Lab2/DemoCalculator/src/main/java/fu/de200053/Calculator.java
@@ -0,0 +1,17 @@
+package fu.de200053;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+            return a / b;
+
+        }
+}
+
